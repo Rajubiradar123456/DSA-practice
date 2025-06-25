17c92d2505ddb558ae8e91357ba15987fb9c0b39
Rajubiradar123456 <rajmbiradar2002@gmail.com>
2025-06-24 23:35:41 -0600

    here the basic loops concept is there

---
+++ loops.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class loops {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in); 
+    
+    // int number=sc.nextInt();
+    // int count=1;
+    // while(count<=number){
+    //     System.out.println("hello world"+count);
+    //     count++;
+    // }
+
+
+    // do while
+    int number=1;
+    do{
+        System.out.println("hello");
+        number++;
+
+    }
+    while(number<0);
+
+}
+}
