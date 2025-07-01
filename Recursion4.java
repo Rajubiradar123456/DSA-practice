24893cc51021b5dd3693cbdb4ee96c100ee1c42d
Rajubiradar123456 <rajmbiradar2002@gmail.com>
2025-07-01 12:55:14 -0600

    Sum of first natural number using recursion

---
+++ Recursion4.java
@@ -0,0 +1,15 @@
+// print sum of first natural nubers
+public class Recursion4 {
+    public static int calsum(int n){
+        if(n==1){
+            return 1;
+        }
+        int fnm1=calsum(n-1);
+        int fn=n+fnm1;
+        return fn;
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(calsum(n));
+    }
+}
