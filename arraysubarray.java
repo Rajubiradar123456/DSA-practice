fec641d443bd4ee70dbe74f6ecd04b9b15e11c3e
Rajubiradar123456 <rajmbiradar2002@gmail.com>
2025-07-05 16:51:46 +0530

    finding the subarray of an array

---
+++ arraysubarray.java
@@ -0,0 +1,17 @@
+public class arraysubarray {
+    public static void subarray(int number[]){
+        for(int i=0;i<number.length;i++){
+            for(int j=i;j<number.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(number[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int number[]={2,4,6,8,10};
+        subarray(number);
+    }
+}
